e7b0d5c71a95a0c65636aee0dc66570b411601e8
Nikita Shagalov <shagalovprog@gmail.com>
2024-02-17 13:43:07 +0300

    PB-37 Add change alarm time handler

--- src/utils/fsm/fsm.py
+++ src/utils/fsm/fsm.py
@@ -40,3 +40,8 @@ class CreateAlarm(StatesGroup):
 class SetNewAlarmRepeatInterval(StatesGroup):
     write_time: State = State()
     accept: State = State()
+
+class SetNewAlarmTime(StatesGroup):
+    write_next_notion_date: State = State()
+    write_next_notion_time: State = State()
+    accept: State = State()
